4aa09d170ccbb4e1c1c6805cb0b03aefd4c3c765
PerScholarSarah <rmandolin22@gmail.com>
2024-08-14 12:56:38 -0400

    implemented attack, takeDamage, and isAlive methods

--- app.js
+++ app.js
@@ -15,16 +15,41 @@ class Spaceship{
 
     // attack enemy spaceship
     attack(target){
+        if(target instanceof Spaceship){
+            console.log(`${this.#name} shoots at ${target.name}...`);
+            
+            const roll = Math.random();
+            if(roll <= this.#stats.accuracy){
+                const dmg = this.#stats.firepower;
+                target.takeDamage(dmg);
+                console.log(`Hit! ${target.name} takes ${dmg} pts. of damage!`);
+            }
+            else{
+                console.log(`Miss! ${target.name} evaded the attack`);
+            }
+        }
         
     }
     // take damage from an attack
     takeDamage(dmg){
-
+        this.#hp -= Math.max(this.#hp, dmg);
+        console.log(`${this.#name}'s hull integrity at ${this.#hp}/${this.#stats.hull}`);
     }
+    // is the ship alive?
+    isAlive(){
+        if(this.#hp <= 0){
+            console.log(`${this.#name} was destroyed!`);
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
     get name(){
         return this.#name;
     }
-    get currentHealth(){
+    get health(){
         return this.#hp;
     }
     get maxHealth(){
